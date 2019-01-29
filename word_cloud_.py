bc275303554ebd6353fd3b00f7b7cf407bc82f17
Harsh <harsh.singh659@gmail.com>
2019-01-29 23:22:18 +0530

    Update word_cloud_.py

--- word_cloud_.py
+++ word_cloud_.py
@@ -20,85 +20,110 @@ def Get_html(url):
         try:
             response = requests.get(url,verify=False)
             html = response.text.encode("utf-8")
-            return str(html)
+            if str(html) is not None:
+                return str(html)
+            else:
+                    pass
         except requests.exceptions.ConnectionError as e:
             print ("connection not establised")
+            print(e)
+            html = "connection not establised"
     else:
         try:
-            url = 'https://'+url
+            url = 'http://'+url
             response = requests.get(url,verify=False)
             html = response.text.encode("utf-8")
-            return str(html)
+            if str(html) is not None:
+                return str(html)
+            else:
+                pass
         except requests.exceptions.ConnectionError as e:
             print ("connection not establised")
-            
+            print(e)
+            html = "connection not establised"
 def cleaning_html(html):
-    text_val = BeautifulSoup(html)
-    for script in text_val(["script","style"]):
-        script.decompose()
-    cleantext_html = ' '.join(text_val.stripped_strings)
-    cachedStopWords = set(stopwords.words("english"))
-    cleantext_html = cleantext_html.lower()
-    cleantext_html = re.sub('\W+',' ',cleantext_html)#removing spsl character
-    cleantext_html = re.sub("^\d+\s|\s\d+\s|\s\d+$"," ",cleantext_html)#removing words ending with number
-    cleantext_html = re.sub(r"(^|\W)\d+","",cleantext_html)#removing words starting with number
-    cleantext_html = re.sub(r'\b\d+(?:\.\d+)?\s+',' ',cleantext_html)#removing numbers
-    cleantext_html = re.sub(r'^[^a-zA-Z]',r' ',cleantext_html)#non words
-    cleantext_html = ' '.join(word for word in cleantext_html.split(' ') if len(word) >1)
-    cleantext_html = ' '.join([word for word in cleantext_html.split() if word not in cachedStopWords]) 
-    return cleantext_html
+    if html is not None:
+        text_val = BeautifulSoup(html)
+        for script in text_val(["script","style"]):
+            script.decompose()
+        cleantext_html = ' '.join(text_val.stripped_strings)
+        cachedStopWords = set(stopwords.words("english"))
+        cleantext_html = cleantext_html.lower()
+        cleantext_html = re.sub('\W+',' ',cleantext_html)#removing spsl character
+        cleantext_html = re.sub("^\d+\s|\s\d+\s|\s\d+$"," ",cleantext_html)#removing words ending with number
+        cleantext_html = re.sub(r"(^|\W)\d+","",cleantext_html)#removing words starting with number
+        cleantext_html = re.sub(r'\b\d+(?:\.\d+)?\s+',' ',cleantext_html)#removing numbers
+        cleantext_html = re.sub(r'^[^a-zA-Z]',r' ',cleantext_html)#non words
+        cleantext_html = ' '.join(word for word in cleantext_html.split(' ') if len(word) >1)
+        cleantext_html = ' '.join([word for word in cleantext_html.split() if word not in cachedStopWords]) 
+        return cleantext_html
+    else:
+        pass
 def word_count(cleantext_html):
-    counts = dict()
-    words =cleantext_html.split()
-    for word in words:
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1
-    return(counts)
-def word_cloud(cleantext_html):            
-    stopwords = set(STOPWORDS)
-    wc = WordCloud(width =800,
-                   height = 800,
-                   background_color = 'white',
-                   max_words=20,
-                   stopwords = stopwords,
-                   min_font_size =10
-                   ).generate(cleantext_html)
-    return wc
+    if cleantext_html is not None:
+        counts = dict()
+        words =cleantext_html.split()
+        for word in words:
+            if word in counts:
+                counts[word] += 1
+            else:
+                counts[word] = 1
+        final_count = dict((key_count, value_count) for key_count, value_count in counts.items() if value_count > 1)
+        return(final_count)
+    else:
+        pass
+def word_cloud(cleantext_html):
+    if cleantext_html is not None:         
+        stopwords = set(STOPWORDS)
+        wc = WordCloud(width =800,
+                       height = 800,
+                       background_color = 'white',
+                       max_words=20,
+                       stopwords = stopwords,
+                       min_font_size =10
+                       ).generate(cleantext_html)
+        return wc
+    else:
+        pass
 def save_img(wc):
-    plt.figure(figsize = (8,8),facecolor =None)
-    plt.imshow(wc)
-    plt.axis("off")
-    plt.tight_layout(pad =0)
-    if os.path.exists("image_data_store"):
-        if os.path.exists("image_data_store/result.png"):
-            plt.savefig('image_data_store/result_{}.png'.format(int(time.time())))
+    if wc is not None:
+        plt.figure(figsize = (8,8),facecolor =None)
+        plt.imshow(wc)
+        plt.axis("off")
+        plt.tight_layout(pad =0)
+        if os.path.exists("image_data_store"):
+            if os.path.exists("image_data_store/result.png"):
+                plt.savefig('image_data_store/result_{}.png'.format(int(time.time())))
+            else:
+                plt.savefig('image_data_store/result.png')
         else:
-            plt.savefig('image_data_store/result.png')
+            os.mkdir("image_data_store")
+            if os.path.exists("image_data_store/result.png"):
+                plt.savefig('image_data_store/result_{}.png'.format(int(time.time())))
+            else:
+                plt.savefig('image_data_store/result.png')
+        plt.show()
     else:
-        os.mkdir("image_data_store")
-        if os.path.exists("image_data_store/result.png"):
-            plt.savefig('image_data_store/result_{}.png'.format(int(time.time())))
-        else:
-            plt.savefig('image_data_store/result.png')
-    plt.show()
+        pass
 def save_file(html):
-    if os.path.exists("text_data_store"):
-        if os.path.exists("text_data_store/website_html_data.txt"):
-            with open('text_data_store/website_html_data{}.txt'.format(int(time.time())),'w') as f:
-                f.write("%s\n" %html)
+    if html is not None:
+        if os.path.exists("text_data_store"):
+            if os.path.exists("text_data_store/website_html_data.txt"):
+                with open('text_data_store/website_html_data{}.txt'.format(int(time.time())),'w') as f:
+                    f.write("%s\n" %html)
+            else:
+                with open('text_data_store/website_html_data.txt','w') as f:
+                    f.write("%s\n" %html)
         else:
-            with open('text_data_store/website_html_data.txt','w') as f:
-                f.write("%s\n" %html)
+            os.mkdir("text_data_store")
+            if os.path.exists("text_data_store/website_html_data.txt"):
+                with open('text_data_store/website_html_data{}.txt'.format(int(time.time())),'w') as f:
+                    f.write("%s\n" %html)
+            else:
+                with open('text_data_store/website_html_data.txt','w') as f:
+                    f.write("%s\n" %html)
     else:
-        os.mkdir("text_data_store")
-        if os.path.exists("text_data_store/website_html_data.txt"):
-            with open('text_data_store/website_html_data{}.txt'.format(int(time.time())),'w') as f:
-                f.write("%s\n" %html)
-        else:
-            with open('text_data_store/website_html_data.txt','w') as f:
-                f.write("%s\n" %html)
+        pass
 def main(url):
     html = Get_html(url)
     save_file(html)
